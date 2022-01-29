4bd43fafcea7e349d67da006c1cbab9ef86f038f
Steph-Roose <steph241291@gmail.com>
2022-01-29 22:15:46 +0100

    finished 1 and 2

--- opdrachten.js
+++ opdrachten.js
@@ -51,7 +51,7 @@ function getTotalSales(arr) {
     for (let i = 0; i < arr.length; i++) {
         total += arr[i];
     }
-    return total;
+    return total.toString();
 }
 
 console.log(getTotalSales(tvSold));
@@ -72,7 +72,7 @@ function getTotalStock(arr) {
     for (let i = 0; i < arr.length; i++) {
         total += arr[i];
     }
-    return total;
+    return total.toString();
 }
 
 console.log(getTotalStock(tvBought));
@@ -84,13 +84,64 @@ totalBought.textContent = getTotalStock(tvBought);
 
 //Opdracht 2e: Geef in het rood weer hoeveel tv's er nog verkocht moeten worden.
 
+let toBeSold = getTotalStock(tvBought) - getTotalSales(tvSold);
+
 const toSell = document.getElementById("to-sell");
-toSell.textContent = getTotalStock(tvBought) - getTotalSales(tvSold);
+toSell.textContent = toBeSold.toString();
 
 //Opdracht 3a: Gebruik een array-methode om alle tv merken (zoals Philips, NIKKEI, etc.) in een lijst op de pagina weer te geven.
 //*Zorg ervoor dat dit ook zou werken als we 200 tv's in onze array zouden hebben staan. Dat er dubbele namen in zitten, is niet erg.
 
+const tvBrands = inventory.map((tv) => {
+    return tv.brand;
+})
+
+console.log(tvBrands);
+
+const brands = document.getElementById("brands");
+
+tvBrands.forEach((tv) => {
+    let li = document.createElement("li");
+    li.innerText = tv;
+    brands.appendChild(li);
+})
 
 //Opdracht 3b: Schrijf de code uit 3a om naar een functie die een array met tv-objecten verwacht.
 //*Het is handig om onze scripts als functies op te zetten, zodat we ze gemakkelijk kunnen hergebruiken.
 //*Tip: vergeet deze functie -declaratie niet aan te roepen!
+
+function inventoryLists(arr, prop) {
+    return arr.map(tv => tv[prop]);
+}
+
+const price = document.getElementById("price");
+price.textContent = inventoryLists(inventory, "price");
+
+//Opdracht 4a: Zorg ervoor dat er een string wordt gegenereerd voor de naam van een tv.
+// Maak een functie die één enkel tv-object (zoals inventory[0] of inventory[6]) verwacht en de naam op de volgende manier samenvoegt:
+// [merk] [type] - [naam] zoals Philips 43PUS6504/12 - 4K TV of NIKKEI NH3216SMART - HD smart TV.
+// Test of jouw functie ook werkt wanneer er een ander tv object wordt meegegeven.
+
+
+
+//Opdracht 4b: Zorg ervoor dat de prijs van een tv netjes geformat wordt. Maak een functie die één tv-prijs als parameter verwacht (zoals 379) en daar de volgende string van maakt: €379,-.
+// Test of jouw functie ook werkt wanneer er een andere tv-prijs wordt meegegeven.
+
+
+
+//Opdracht 4c: Zorg ervoor dat er een string wordt gegenereerd voor alle beschikbare schermgroottes van één tv, in zowel inches als cm.
+// Doe dit door een functie te schrijven die één screen-sizes array verwacht ( zoals inventory[0].availableSizes) en de groottes op de volgende manier samenvoegt:
+// [schermgrootte] inches ([schermgrootte omgerekend]cm) | [schermgrootte] inches ([schermgrootte omgerekend]cm) etc.
+// Als een tv maar één schermgrootte heeft ([32]) wordt de output 32 inch (81 cm). Wanneer een tv vier schermgroottes heeft ([43, 50, 55, 58]) wordt de output 43 inch (109 cm) | 50 inch (127 cm) | 58 inch (147 cm).
+// Test of jouw functie werkt in alle mogelijke gevallen.
+
+
+
+//Opdracht 4d: Zorg ervoor de informatie van één van de tv's zoals het voorbeeld wordt weergegeven op de pagina. Gebruik hiervoor de functies die je hebt gemaakt in opdracht 5a, 5b en 5c.
+
+
+
+//Opdracht 4e: Schrijf een functie die ALLE tv's weergeeft op de pagina zoals in het voorbeeld.
+// Dit wil je natuurlijk niet acht keer opnieuw schrijven, want nu zijn het 8 tv's, maar in de toekomst misschien wel 200!
+// Gebruik in deze functie de voorgaande functies die je hebt geschreven, om onderdelen van de data te formatten.
+// De overkoepelende "tv-generator-functie" verwacht één parameter: de volledige array met tv-objecten. Vergeet 'm niet aan te roepen!
